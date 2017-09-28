822227f04d256dfdde61d19ab5345de0c95d24db
Peter Wu <piterwu@outlook.com>
2017-09-28 21:44:56 +0800

    文件夹权限

--- im/entrypoint.sh
+++ im/entrypoint.sh
@@ -3,13 +3,13 @@
 groupmod -o -g $AUDIO_GID audio
 groupmod -o -g $VIDEO_GID video
 
-if [ $UID != $(echo `id -u qq`) -o $GID != $(echo `id -g qq`) ]; then
+if [ $GID != $(echo `id -g qq`) ]; then
     groupmod -o -g $GID qq
-    if [ $UID != $(echo `id -u qq`) ]; then
-        usermod -o -u $UID qq
-    fi
-    chown -R qq:qq /TencentFiles
 fi
+if [ $UID != $(echo `id -u qq`) ]; then
+    usermod -o -u $UID qq
+fi
+chown qq:qq /TencentFiles
 
 su qq <<EOF
 if [ "$1" ]; then
