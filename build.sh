082bdf06e3e8c356a05fc092bd306afadfde484d
Peter Wu <piterwu@outlook.com>
2017-10-08 19:43:38 +0800

    修改build

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd office
-docker build -t bestwu/qq:office .
-cd ../im
-docker build -t bestwu/qq:im .
-cd ../im.light
-docker build -t bestwu/qq:light -t bestwu/qq .
+docker build -t bestwu/qq:office ./office
+docker build -t bestwu/qq:im ./im
+docker build -t bestwu/qq:light -t bestwu/qq ./im.light
