6e17cf5a1c452b9d2afe1f96cc25810f1630148b
Peter Wu <piterwu@outlook.com>
2019-02-14 16:10:28 +0800

    调整启动脚本

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,29 +12,18 @@ fi
 chown qq:qq /TencentFiles
 
 su qq <<EOF
-
-function _exist(){
-    if test $( pgrep -f $1 | wc -l ) -eq 0
-    then
-	echo "退出"
-    else
-	sleep 60
-        _exist $1
-    fi 
-}
-
-if [ "$1" ]; then
-    echo "deepin-wine $1"
-    deepin-wine $1
-else
-    echo "启动 $APP"
-    mkdir -p /home/qq/.deepinwine
-    touch /home/qq/.deepinwine/.QQ_run
-    "/opt/deepinwine/apps/Deepin-$APP/run.sh"
-    #tail -fn 0 /home/qq/.deepinwine/.QQ_run
-    sleep 300
-    _exist "QQProtect"
-fi
-
-exit 0
+   echo "启动 $APP"
+   mkdir -p /home/qq/.deepinwine
+   touch /home/qq/.deepinwine/.QQ_run
+   "/opt/deepinwine/apps/Deepin-$APP/run.sh"
 EOF
+
+#tail -fn 0 /home/qq/.deepinwine/.QQ_run
+echo $(pgrep QQProtect.exe | wc -l ) 
+sleep 300
+while [ $(pgrep QQProtect.exe | wc -l ) -ne 0 ]
+do
+    echo $(pgrep QQProtect.exe | wc -l )
+    sleep 60
+done
+echo "退出"
